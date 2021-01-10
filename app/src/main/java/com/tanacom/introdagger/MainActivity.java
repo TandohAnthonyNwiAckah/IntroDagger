cbcf05e84f2b53c666fed517ef56fc6f9aec3ef3
TANACOM <tanamoinc@gmail.com>
2021-01-10 12:50:48 +0000

    commit Dagger Component and Constructor injecction

---
+++ app/src/main/java/com/tanacom/introdagger/MainActivity.java
@@ -0,0 +1,38 @@
+package com.tanacom.introdagger;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tanacom.introdagger.interfaces.CarComponent;
+import com.tanacom.introdagger.interfaces.DaggerCarComponent;
+import com.tanacom.introdagger.models.Car;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Car car;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        initCar();
+
+    }
+
+
+
+    private void initCar() {
+        CarComponent carComponent = DaggerCarComponent.create();
+
+        car = carComponent.getCar();
+
+        car.drive();
+
+    }
+
+
+}
