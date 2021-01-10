cd3cb7cbab4fc6eaa909458177b8135eeb2f3b33
TANACOM <tanamoinc@gmail.com>
2021-01-10 16:28:40 +0000

    commit method injection.

--- app/src/main/java/com/tanacom/introdagger/models/Car.java
+++ app/src/main/java/com/tanacom/introdagger/models/Car.java
@@ -6,22 +6,39 @@ import javax.inject.Inject;
 
 public class Car {
 
+    // Constructor are injected first followed by field before method.
+
     private static final String TAG = "Car";
 
     //  Engine and Wheel are dependencies of cars. Hence Car depends on them.
+//    @Inject
+
     private Engine engine;
     private Wheel wheel;
 
     // Constructor Injection is when a dependencies of a class is pass to its constructor.
+//    @Inject
+//    public Car(Wheel wheel) {
+//        this.wheel = wheel;
+//    }
+
+
     @Inject
     public Car(Engine engine, Wheel wheel) {
         this.engine = engine;
         this.wheel = wheel;
     }
 
-    public void drive() {
-        Log.d(TAG, "driving");
+
+    @Inject
+    public void enableRemote(Remote remote) {
+        remote.setListener(this);
 
     }
+
+    public void drive() {
+        Log.d(TAG, "driving");
+    }
+
 
 }
